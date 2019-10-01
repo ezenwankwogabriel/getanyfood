0143ee41499a352839222284f906230a9bfdd378
Tobalase Akinyemi <funspectre@users.noreply.github.com>
2019-10-01 23:14:13 +0100

    Format product model

--- models/product/index.js
+++ models/product/index.js
@@ -30,7 +30,10 @@ const productSchema = new Schema({
   calories: Number,
   discount: { type: Number, min: 0, max: 100 },
   rating: {
-    type: Number, min: 0, max: 5, default: 0,
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0,
   },
   deliveryTime: { type: Number, required: true },
   deliveryType: { type: String, enum: ['free', 'paid'], default: 'paid' },
